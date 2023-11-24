f76a77cd29520510f5d389da1a9351b597e6b465
Carlos-Tucche <carlos.dias2021@outlook.com>
2023-11-24 18:06:24 -0300

    inicio dia 24.10

--- 09-ProjetoCompartilhado/src/br/com/treinamento/Principal.java
+++ 09-ProjetoCompartilhado/src/br/com/treinamento/Principal.java
@@ -8,13 +8,13 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Double circunferencia = Calculos.calcularCircunferencia(5);
-		System.out.println("A circunferencia é: \n"+ circunferencia );
+		System.out.println("A circunferencia é: "+ circunferencia );
 		
 		Integer soma = Calculos.somarDoisNumeros(7,3);
-		System.out.println("A soma de 7 e 3  é: \n"+soma);
+		System.out.println("A soma de 7 e 3  é: "+soma);
 		
 		Integer area = Calculos.calcularAreaQuadrado(5, 3);
-		System.out.println("A area de um quadrado de A= 5 e L= 3 é: \n"+area);
+		System.out.println("A area de um quadrado de A= 5 e L= 3 é: "+area);
 
 		String ponto = AuxiliarString.removePontuacao("217.555.482");
 		System.out.println("Número sem pontuação; "+ponto);
---
+++ 10-ImportandoProjeto/src/br/com/treinamento/Principal.java
@@ -0,0 +1,14 @@
+package br.com.treinamento;
+
+import br.com.treinamento.calculos.Calculos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Integer soma = Calculos.somarDoisNumeros(5,20);
+		System.out.println("A soma de 5 e 20 é: "+soma);
+
+	}
+
+}
