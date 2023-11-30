e34233cd04c2165e17abc5b8849a60ec1452c87a
Carlos-Tucche <carlos.dias2021@outlook.com>
2023-11-30 18:24:37 -0300

    Biblioteca atualizada

--- Revisap2/src/main/java/br/com/treinamento/Revisap2/App.java
+++ Revisap2/src/main/java/br/com/treinamento/Revisap2/App.java
@@ -1,13 +1,22 @@
 package br.com.treinamento.Revisap2;
 
-/**
- * Hello world!
- *
- */
+import br.com.treinamento.Revisap2.model.Biblioteca;
+import br.com.treinamento.Revisap2.model.Livro;
+import br.com.treinamento.Revisap2.model.Usuario;
+import lombok.ToString;
+
+@ToString
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Usuario usuario1 = new Usuario("Carlos", 43, 9123456, "cad@gmail.com");
+    	Livro livro1 = new Livro("Braz Cubas", "Machado de Assis", "Disponivel");
+    	
+    	Biblioteca biblioteca = new Biblioteca();
+    	biblioteca.getUsuario(usuario1);
+    	biblioteca.getLivro(livro1);
+    	
+    	System.out.println("O livro esta, "+ livro1.getEmprestimo());
     }
 }
--- Revisap2/src/main/java/br/com/treinamento/Revisap2/model/Biblioteca.java
+++ Revisap2/src/main/java/br/com/treinamento/Revisap2/model/Biblioteca.java
@@ -1,6 +1,25 @@
 package br.com.treinamento.Revisap2.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
 public class Biblioteca {
 
+	@Getter @Setter
+	private Livro livro;
 	
+	@Getter @Setter
+	private Usuario usuario;
+
+	public void getLivro(Livro livro1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getUsuario(Usuario usuario1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
