b2f05c2b661929815261a3679e3cae43aed7bb7b
Carlos-Tucche <carlos.dias2021@outlook.com>
2023-12-06 18:26:04 -0300

    Termino do exercicio Revisão2

--- Revisap2/src/main/java/br/com/treinamento/Revisap2/App.java
+++ Revisap2/src/main/java/br/com/treinamento/Revisap2/App.java
@@ -13,6 +13,7 @@ public class App
     	Usuario usuario1 = new Usuario("Carlos", 43, 9123456, "cad@gmail.com");
     	Livro livro1 = new Livro("Braz Cubas", "Machado de Assis", "Disponivel");
     	Livro livro2 = new Livro("O Cortiço","Aluizio de Azevedo", "Emprestado");
+    	Livro livro3 = new Livro("A mão e a Luva", "Machado de Assis", "Entregue");
     	
     	Biblioteca biblioteca = new Biblioteca();
     	biblioteca.setUsuario(usuario1);
@@ -20,7 +21,7 @@ public class App
     	
     	System.out.println("O livro "+ livro1.getTitulo()+" esta: "+ livro1.getEmprestimo());
     	System.out.println("O livro "+ livro2.getTitulo()+" foi emprestado para: "+usuario1.getNome());
-    	System.out.println("O livro ");
-    	
+    	System.out.println("O livro "+ livro2.getTitulo()+" tem status de: "+ livro2.getEmprestimo());
+    	System.out.println("o livro "+ livro3.getTitulo()+" esta com status de: "+ livro3.getEmprestimo());
     }
 }
