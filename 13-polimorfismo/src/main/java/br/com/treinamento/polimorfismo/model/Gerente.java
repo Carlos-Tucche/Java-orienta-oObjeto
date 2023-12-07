1767563bc4a619ce8de3b698d58e8bfaef0aa7a6
Carlos-Tucche <carlos.dias2021@outlook.com>
2023-12-07 18:56:01 -0300

    Revisão2 e Polimofismo

---
+++ 13-polimorfismo/src/main/java/br/com/treinamento/polimorfismo/model/Gerente.java
@@ -0,0 +1,18 @@
+package br.com.treinamento.polimorfismo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Gerente extends Funcionario {
+
+	@Getter @Setter
+	private Integer senha;
+	
+	@Override
+	public Double getBonificacao() {
+		return super.getBonificacao() + super.getSalario();
+	}
+	public Boolean autentica (Integer senha) {
+		return(this.senha==senha);
+	}
+}
---
+++ 13-polimorfismo/src/main/java/br/com/treinamento/polimorfismo/service/ControleBonificacao.java
@@ -0,0 +1,21 @@
+package br.com.treinamento.polimorfismo.service;
+
+import br.com.treinamento.polimorfismo.model.Funcionario;
+import lombok.Getter;
+
+public class ControleBonificacao {
+	
+	@Getter
+	private Double soma = 0.0;
+	
+	public void registra (Funcionario funcionario) {
+		soma += funcionario.getBonificacao();
+	}
+	
+	/*
+	 * this.soma = gerente.getBonificacao();
+	 * 
+	 * Double boni = gerente.getBonificacao();
+		this.soma = this.soma + boni;
+	 * */
+}
