9001677f6548107cf54fbd6160ae9bf858485520
Carlos-Tucche <carlos.dias2021@outlook.com>
2023-12-08 21:22:11 -0300

    equações bigdecimal

--- 18-BigDecimal/src/br/com/treinamento/Principal.java
+++ 18-BigDecimal/src/br/com/treinamento/Principal.java
@@ -7,11 +7,34 @@ public class Principal {
 	public static void main(String[] args) {
 
 		
-		BigDecimal big1 = new BigDecimal("0.3");
+		BigDecimal big1 = new BigDecimal("50.1");
+		BigDecimal big2 = new BigDecimal("2");
 		
+		System.out.println("Adição / soma.");
 		System.out.println(big1.add(big1).add(big1));
 		
-		System.out.println(big1.multiply(new BigDecimal("3")));
+		System.out.println("Subtrair");
+		System.out.println(big1.subtract(big2));
+		
+		System.out.println("Compara");
+		System.out.println(big1.compareTo(big2));
+		
+		System.out.println("Divisão");
+		System.out.println(big1.divide(big2));
+		
+		System.out.println("Maximo");
+		System.out.println(big1.max(big2));
+		
+		System.out.println("Minimo");
+		System.out.println(big1.min(big2));
+		
+		System.out.println("Potencia");
+		System.out.println(big1.pow(4));
+		
+		System.out.println("Multiplicação");
+		System.out.println(big1.multiply(big2));
+		
+		//*System.out.println(big1.multiply(new BigDecimal("3")));
 	}
 
 }
